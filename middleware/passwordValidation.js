a4c5d3f20ff6dcedf881bcb435ce9c1bf9d9d786
Lu-Tola07 <tola.praiz@gmail.com>
2024-09-13 13:33:26 +0100

    Corrections to Password Validation 2.

--- middleware/passwordValidation.js
+++ middleware/passwordValidation.js
@@ -1,25 +1,31 @@
 const Joi = require("@hapi/joi");
 
 const schema = Joi.object({
-    password: Joi.string()
-    .required()
+    // password: Joi.string()
+    // .required()
+    // .pattern(new RegExp("^(?=.*[!@#$%^&*])(?=.*[A-Z]).{8,}$"))
+    // .messages({
+    //   "any.required": "Please provide a password.",
+    //   "string.empty": "Password cannot be left empty.",
+    //   "string.pattern.base": "Password must be at least 8 characters long and include at least one uppercase letter and one special character (!@#$%^&*).",
+    // }),
+    oldPassword: Joi.string().required().messages({
+        'string.empty': 'Please provide your old password.',
+        'any.required': '"oldPassword" is not allowed'
+    }),
+    newPassword: Joi.string().required()
     .pattern(new RegExp("^(?=.*[!@#$%^&*])(?=.*[A-Z]).{8,}$"))
     .messages({
       "any.required": "Please provide a password.",
       "string.empty": "Password cannot be left empty.",
       "string.pattern.base": "Password must be at least 8 characters long and include at least one uppercase letter and one special character (!@#$%^&*).",
     }),
-    oldPassword: Joi.string().required().messages({
-        'string.empty': 'Please provide your old password.',
-        'any.required': '"oldPassword" is not allowed'
-    }),
-    newPassword: Joi.string().required().messages({
-        'string.empty': 'Please provide a new password.',
-        'any.required': '"newPassword" is not allowed'
-    }),
-    confirmNewPassword: Joi.string().required().messages({
-        'string.empty': 'Please confirm your new password.',
-        'any.required': '"confirmNewPassword" is not allowed'
+    confirmNewPassword: Joi.string().required()
+    .pattern(new RegExp("^(?=.*[!@#$%^&*])(?=.*[A-Z]).{8,}$"))
+    .messages({
+      "any.required": "Please provide a password.",
+      "string.empty": "Password cannot be left empty.",
+      "string.pattern.base": "Password must be at least 8 characters long and include at least one uppercase letter and one special character (!@#$%^&*).",
     })
 });
 
