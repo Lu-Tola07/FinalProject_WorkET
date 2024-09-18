23a9d157f4b2bc912ae89ab60a11df54e05a930e
Lu-Tola07 <tola.praiz@gmail.com>
2024-09-18 16:11:45 +0100

    Made corrections to Server 2.

--- server.js
+++ server.js
@@ -3,27 +3,25 @@ const express = require('express');
 const cors = require('cors');
 const staffRouter = require('./router/staffRouter');
 const userRouter = require('./router/userRouter');
-const keepServerAlive = require('./keepServerAlive');
+// const keepServerAlive = require('./keepServerAlive');
 
 
 const port = process.env.PORT || 4400;
 
 const app = express();
 
-app.use(cors({
-    origin: "*"
-}));
+app.use(cors("*"));
 app.use(express.json());
 
 app.get('/', (req, res) => {});
 app.use('/api/v1', staffRouter, userRouter);
 
-keepServerAlive();
+// keepServerAlive();
 
 
-app.get('/1', (req, res) => {
-    res.send('Server is alive!');
-});
+// app.get('/1', (req, res) => {
+//     res.send('Server is alive!');
+// });
 
 app.listen(port, () => {
     console.log(`Server is running on PORT: ${port}`);
